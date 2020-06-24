d8ba458140601992726973a42a4799d18c696399
tokoroten-lab <tokoroten.lab@gmail.com>
2020-06-25 04:53:18 +0900

    Update main function

--- part-3/last-work/server.go
+++ part-3/last-work/server.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	"cs-experiment-1/part-3/last-work/geotag"
 	"cs-experiment-1/part-3/last-work/tag"
+	"fmt"
 )
 
 func main() {
-	geotags, err := geotag.LoadGeoTags("samples/geotag.csv")
+	geotags, err := geotag.ReadGeoTagsFromCSV("samples/huge_geotag.csv")
 	if err != nil {
 		panic(err)
 	}
 
-	tags, err := tag.LoadTags("samples/tag.csv")
+	tags, err := tag.ReadTagsFromCSV("samples/huge_tag.csv")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(geotags)
-	fmt.Println(tags)
+	fmt.Println("Length of geotags", len(geotags))
+	fmt.Println("Length of tags", len(tags))
 }
