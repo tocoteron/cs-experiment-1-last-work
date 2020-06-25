e8c1af1d931de485354af3e9e6efb2aa8d94b68b
tokoroten-lab <tokoroten.lab@gmail.com>
2020-06-25 15:24:45 +0900

    Add web server system

--- part-3/last-work/server.go
+++ part-3/last-work/server.go
@@ -3,10 +3,59 @@ package main
 import (
 	"cs-experiment-1/part-3/last-work/geotag"
 	"cs-experiment-1/part-3/last-work/tag"
+	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo"
 )
 
+// TemplateRenderer is a view templates renderer
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func startWebServer(port string, tagSearchTable map[string][]*geotag.GeoTag) {
+	e := echo.New()
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("view/*.html")),
+	}
+	e.Renderer = renderer
+
+	e.GET("/search", func(c echo.Context) error {
+		tag := c.QueryParam("tag")
+		geotags := searchGeoTagsByTag(tagSearchTable, tag)
+
+		return c.Render(http.StatusOK, "search.html", map[string]interface{}{
+			"geotags": geotags,
+		})
+	})
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
+}
+
+func searchGeoTagsByTag(tagSearchTable map[string][]*geotag.GeoTag, tag string) []geotag.GeoTag {
+	geotagPointers := tagSearchTable[tag]
+	geotags := make([]geotag.GeoTag, len(geotagPointers))
+
+	for i := 0; i < len(geotags); i++ {
+		geotags[i] = *geotagPointers[i]
+	}
+
+	return geotags
+}
+
 func main() {
+	port := flag.String("port", "1323", "Port number of web server")
+	flag.Parse()
+
 	geotags, err := geotag.ReadGeoTagsFromCSV("samples/geotag.csv")
 	if err != nil {
 		panic(err)
@@ -32,16 +81,5 @@ func main() {
 		tagSearchTable[tags[i].Tag] = append(tagSearchTable[tags[i].Tag], geotagsPointerTable[tags[i].ID])
 	}
 
-	var searchTag string
-
-	for {
-		fmt.Print("Search: ")
-		fmt.Scan(&searchTag)
-
-		if searchTag == "exit" || searchTag == "quit" {
-			break
-		}
-
-		fmt.Println(tagSearchTable[searchTag])
-	}
+	startWebServer(*port, tagSearchTable)
 }
