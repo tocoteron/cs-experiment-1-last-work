62deb30fd16b0c8937785d9e77e6962f7c6b62f3
tokoroten-lab <tokoroten.lab@gmail.com>
2020-07-20 20:50:26 +0900

    Add tag search table generator

---
+++ tag-search-table-generator.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"sort"
+
+	//	"github.com/tokoroten-lab/cs-experiment-1-last-work/geotag"
+	"github.com/tokoroten-lab/cs-experiment-1-last-work/geotag"
+	"github.com/tokoroten-lab/cs-experiment-1-last-work/tag"
+)
+
+func main() {
+	geotags, err := geotag.ReadGeoTagsFromCSV("data/geotag.csv", 10400000, 10000)
+	if err != nil {
+		panic(err)
+	}
+
+	tags, err := tag.ReadTagsFromCSV("data/tag.csv", 22820000, 10000)
+	if err != nil {
+		panic(err)
+	}
+
+	idSearchTable := geotag.IDSearchTable{}
+
+	for i := 0; i < len(geotags); i++ {
+		idSearchTable[geotags[i].ID] = &geotags[i]
+	}
+
+	tagSearchTable := make(geotag.TagSearchTable, len(tags))
+
+	for _, tag := range tags {
+		tagSearchTable[tag.Tag] = append(tagSearchTable[tag.Tag], idSearchTable[tag.ID])
+	}
+
+	for _, geotags := range tagSearchTable {
+		sort.Slice(geotags, func(i, j int) bool {
+			return geotags[i].Time > geotags[j].Time
+		})
+
+		if len(geotags) > 100 {
+			geotags = geotags[:100]
+		}
+	}
+
+	geotag.WriteTagSearchTableToCSV("data/tag-search-table.csv", tagSearchTable)
+}
